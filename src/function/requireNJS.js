b325b5f860dff3d914213dd0940284ff20ec11cb
Adrien Risser <risseraka@gmail.com>
2013-02-11 01:17:52 +0100

    adding fs.watchFile coupled with FORCE_NJS_RELOAD config

--- src/function/requireNJS.js
+++ src/function/requireNJS.js
@@ -1,13 +1,28 @@
 
+var watchedFiles = Object.create(null),
+    // prevent prototype hacks
+    prefix = '_';
+
 // used to require .njs files
 function requireNJS() {
-  // invalidate the module
-  if (FORCE_NJS_RELOAD && require.cache.hasOwnProperty(this.path)) {
-    delete require.cache[this.path];
-  }
   try {
+    var path = this.path,
+        prefixedPath = prefix + path,
     // not my fault if require is synchronous ...
-    var module = require(this.path);
+        module = require(path);
+
+    if (FORCE_NJS_RELOAD && !watchedFiles[prefixedPath]) {
+        console.log('init watch on:', path);
+        watchedFiles[prefixedPath] = true;
+        // fs.watch seems to be a bit too aggressive with reloading
+        // reloads twice when saving once in text editor
+        fs.watchFile(path, function(event) {
+            console.log('removing from cache:', path);
+            if (require.cache.hasOwnProperty(path)) {
+                delete require.cache[path];
+            }
+        });
+    }
   } catch(o_O) {
     console.error(o_O);
     return internalServerError.call(this, o_O);
